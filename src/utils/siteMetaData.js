b5a396866757ed9375625fba8195a86a4219fce7
lucky verma <aadyant001001@gmail.com>
2024-04-27 12:08:43 +0530

    last changes

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+    author: 'Lucky',
+    headerTitle: 'Next.js Blog',
+    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'aadyant001001@gmail.com', 
+    github: 'https://github.com/thisnotebook',
+    twitter: 'https://twitter.com/',
+    facebook: 'https://facebook.com',
+    youtube: 'https://youtube.com/',
+    linkedin: 'https://www.linkedin.com/in/lucky-verma-109921228/',
+    dribbble: 'https://www.google.com',
+    locale: 'en-US',
+  }
+  
+  module.exports = siteMetadata
